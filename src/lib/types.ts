e0f54071f897841b86d82f9d9d71cf90a441a5be
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2024-04-28 21:16:31 +0200

    refactor: remove unsed types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -15,30 +15,4 @@ export type AnnotationType = {
   };
 };
 
-//@TODO: refactor: use this type for tool in all of the app
 export type Tool = "rectangle" | "circle" | "select";
-
-//TODO: to use after refactor
-export interface AnnotationBase {
-  id: number;
-  shapeType: string;
-  label: string;
-  editing: boolean;
-}
-
-export interface RectangleAnnotation extends AnnotationBase {
-  shapeData: {
-    x: number;
-    y: number;
-    width: number;
-    height: number;
-  };
-}
-
-export interface CircleAnnotation extends AnnotationBase {
-  shapeData: {
-    centerX: number;
-    centerY: number;
-    radius: number;
-  };
-}
